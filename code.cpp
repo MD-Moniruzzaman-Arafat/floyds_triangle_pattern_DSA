ef04c2972d21c55d673a557af947efdfab3b7465
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-09-17 21:14:13 +0600

    HW

--- code.cpp
+++ code.cpp
@@ -1,22 +1,39 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int number = 1;
+//     cout << "number of n : ";
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i + 1; j++)
+//         {
+//             cout << number << " ";
+//             number++;
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int n;
-    int number = 1;
-    cout << "number of n : ";
-    cin >> n;
-
+    char ch = 'A';
+    int n = 4;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i + 1; j++)
         {
-            cout << number << " ";
-            number++;
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
-
-    return 0;
 }
